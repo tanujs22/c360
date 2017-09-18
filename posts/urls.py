c7c2271ad183bf73db8c7ae6da3550872c271e9b
Tanuj Srivastava <srim0987@gmail.com>
2017-09-18 17:03:26 +0530

    f

--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     url(r'^create/$', views.post_create),
     url(r'^(?P<id>\d+)/$', views.post_detail, name="detail"),
     url(r'^(?P<id>\d+)/edit/$', views.post_update, name='update'),
-    # url(r'^(?P<id>\d+)/vote/', views.vote, name='vote'),
     url(r'^(?P<id>\d+)/delete/$', views.post_delete),
 ]
